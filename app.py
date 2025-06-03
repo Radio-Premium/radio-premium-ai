2270b228de49b1564f5b5a138ed36a31d1a16d50
h-faber <hfaberlu@gmail.com>
2025-06-03 22:24:26 +0900

    feat: transcribe 라우터 등록

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from routes.transcribe import transcribe_bp  # 1. transcribe 라우터 가져오기
 
 app = Flask(__name__)
+app.register_blueprint(transcribe_bp)
 
 @app.route("/")
 def index():
-    return "Whisper 서버 초기 실행 성공!"
+  return "Whisper 서버 초기 실행 성공!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+  app.run(host="0.0.0.0", port=5000)
