520c53cf42274d1746043b3e3a62d428ee3ba8ec
DaEun <129145396+Eun0713@users.noreply.github.com>
2025-06-04 22:19:52 +0900

    feat: 라디오 자막 추출 결과 서버로 전송 처리 구현 (#8)
    * feat: 라디오 자막 추출 결과 서버로 전송 처리 구현
    
    - socketio 사용
    - Express 기반 node.js 서버로 전송
    
    * chore: socketio 실행을 위한 의존성 패키지 추가
    
    - python-socketio 추가
    - 정렬 변경
    
    ---------
    
    Co-authored-by: 오희주 <hfaberlu@gmail.com>

--- services/whisper_service.py
+++ services/whisper_service.py
@@ -1,3 +1,4 @@
+import socketio
 import subprocess
 import whisper
 import numpy
@@ -6,6 +7,10 @@ import queue
 
 model = whisper.load_model("base", device="cpu")
 text_queue = queue.Queue()
+
+socketio = socketio.Client()
+# TODO: Whisper 서버 배포 시 주소 변경
+socketio.connect("http://localhost:3000")
 
 def transcribe_radio_stream(url):
   def worker():
@@ -32,8 +37,10 @@ def transcribe_radio_stream(url):
         audio = numpy.frombuffer(data, numpy.int16).astype(numpy.float32) / 32768.0
         result = model.transcribe(audio, language="ko")
         text = result.get("text", "").strip()
+
         if text:
-          text_queue.put(text)
+          socketio.emit("transcribedRadioText", {"text": text})
+          print(f"[전송됨] {text}")
 
     except Exception as e:
       print(f"[whisper_service] 오류: {e}")
